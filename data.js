07556440629637b08a40155d73c8ac7fd15cb898
h3rb <2787368+h3rb@users.noreply.github.com>
2024-09-24 17:27:23 -0400

    Create data.js

---
+++ data.js
@@ -0,0 +1,24 @@
+
+let indiedevs = [
+
+  /* Try using the template found in the Readme here: https://github.com/LAGameStudio/lagamestudio.github.io/ */
+
+ { 
+   name: "Lost Astronaut Studios",
+   date: "9-24-2024",
+   linkback: "https://lostastronaut.com", 
+   links: [
+     { kind: "discord", url: "https://discord.gg/9vkNPJr" },
+     { kind: "steam", url: "https://store.steampowered.com/dev/lost" },
+     { kind: "facebook", url: "https://www.facebook.com/lostastro/" },
+     { kind: "github", url: "https://github.com/LAGameStudio" },
+     { kind: "twitter", url: "x.com/LAGameStudio" },
+     { kind: "twitch", url: "https://www.twitch.tv/lostastronautstudios" },
+     { kind: "youtube", url: "https://www.youtube.com/@LAGameStudio/playlists" }
+   ],
+   paragraph: "Lost Astronaut Studios is a Pittsburgh-based independent game company founded by a Carnegie Mellon alum in 2006, and continues producing retro-inspired science fiction games.  Support local and independent software producers like Lost Astronaut by purchasing Apolune 2 on Steam. Join our Discord to interact more closely with us and our community. Lost Astronaut Studios produces open source software to help the junior and independent game developer community.",
+   channel: "#lost-astronaut", 
+   image: "images/LALogo.png"
+ }
+
+]
