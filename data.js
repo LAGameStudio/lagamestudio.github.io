111434aa550efbae76f6695f372bddc286e07c59
h3rb <2787368+h3rb@users.noreply.github.com>
2024-09-25 11:54:43 -0400

    Update data.js

--- data.js
+++ data.js
@@ -2,10 +2,42 @@
 let indiedevs = [
 
   /* Try using the template found in the Readme here: https://github.com/LAGameStudio/lagamestudio.github.io/ */
-
  { 
-   name: "Lost Astronaut Studios",
+   kind: "tool",
+   title: "InputCandy",
+   image: "images/InputCandyTeaser.png",   
+   date: "9-25-2024",
+   location: "United States",
+   channel: "#lost-astronaut".
+   linkback: "https://github.com/LAGameStudio/InputCandy", 
+   links: [
+     { kind: "discord", url: "https://discord.gg/9vkNPJr" },
+   ],
+   paragraph: "Awesome gamepad support for GameMaker Studio 2 games. InputCandy focuses on providing input testing and customization for users of PC and gamepad-friendly platforms (consoles). It's that classic 'control setup' panel system you are accustomed to seeing in video games small and large, but for GameMaker. It also is an object-oriented input API, with action matching, and a bunch of device detection features for keyboard, mouse and any SDL controller. For GMS 2.3.1+"
+ },
+  
+ { 
+   kind: "game",
+   title: "Apolune 2",
+   image: "images/LALogo.png",   
    date: "9-24-2024",
+   location: "United States",
+   channel: "#lost-astronaut".
+   linkback: "https://lostastronaut.com/apolune2", 
+   links: [
+     { kind: "discord", url: "https://discord.gg/9vkNPJr" },
+     { kind: "steam", url: "https://store.steampowered.com/app/1549140/Apolune_2/" },
+   ],
+   paragraph: "Apolune 2 is a hilarious and shocking resource management game where one player (or more) experience life on an asteroid mining team in deep space. Tethered to a heavy space station, the game evolves as you mine ore for money, trade and upgrade your station, alone or with your friends."
+ },
+  
+ { 
+   kind: "company",
+   title: "Lost Astronaut Studios",
+   image: "images/LALogo.png",   
+   date: "9-24-2024",
+   location: "United States",
+   channel: "#lost-astronaut".
    linkback: "https://lostastronaut.com", 
    links: [
      { kind: "discord", url: "https://discord.gg/9vkNPJr" },
@@ -16,9 +48,7 @@ let indiedevs = [
      { kind: "twitch", url: "https://www.twitch.tv/lostastronautstudios" },
      { kind: "youtube", url: "https://www.youtube.com/@LAGameStudio/playlists" }
    ],
-   paragraph: "Lost Astronaut Studios is a Pittsburgh-based independent game company founded by a Carnegie Mellon alum in 2006, and continues producing retro-inspired science fiction games.  Support local and independent software producers like Lost Astronaut by purchasing Apolune 2 on Steam. Join our Discord to interact more closely with us and our community. Lost Astronaut Studios produces open source software to help the junior and independent game developer community.",
-   channel: "#lost-astronaut", 
-   image: "images/LALogo.png"
+   paragraph: "Lost Astronaut Studios is a Pittsburgh-based independent game company founded by a Carnegie Mellon alum in 2006, and continues producing retro-inspired science fiction games.  Support local and independent software producers like Lost Astronaut by purchasing Apolune 2 on Steam. Join our Discord to interact more closely with us and our community. Lost Astronaut Studios produces open source software to help the junior and independent game developer community."
  }
 
 ]
