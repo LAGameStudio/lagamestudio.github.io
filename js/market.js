3887ee1a430e9fe99311b2d89a3f4fd440a97063
h3rb <2787368+h3rb@users.noreply.github.com>
2024-09-25 11:00:47 -0400

    Create market.js

---
+++ js/market.js
@@ -0,0 +1,38 @@
+/*
+            <div class="row d_flex">
+                     <div class="col-md-6">
+                        <div class="about_img">
+                           <figure><img src="images/about_img.jpg" alt="#"/></figure>
+                        </div>
+                     </div>
+                     <div class="col-md-6">
+                        <div class="titlepage">
+                           <h2>About <span class="blu">us</span></h2>
+                           <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters,</p>
+                           <a class="read_more">Visit</a>
+                              <div class="col-sm-col-xl-6 col-lg-6 col-md-6 col-sm-12">
+                                 <ul class="social_icon">
+                                    <li><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+                                    <li><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+                                    <li><a href="#"><i class="fa fa-linkedin-square" aria-hidden="true"></i></a></li>
+                                    <li><a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
+                                 </ul>
+                              </div>
+                        </div>
+                     </div>
+                  </div>
+*/
+function IndieDevSpace() {
+var companies=document.getElementById("companies");
+
+var games=document.getElementById("games");
+  
+var tools=document.getElementById("tools");
+
+indiedevs.forEach((e)=> {
+  if ( e.type == "company" ) {
+  } if ( e.type == "game" ) {
+  } else { // tools and open source
+  }
+});
+}
