fb35a27f018a476711ab30a30eb8f423d76f344f
h3rb <2787368+h3rb@users.noreply.github.com>
2024-09-25 12:23:05 -0400

    Update data.js

--- data.js
+++ data.js
@@ -11,6 +11,7 @@ let indiedevs = [
    channel: "#lost-astronaut",
    linkback: "https://github.com/LAGameStudio/InputCandy", 
    links: [
+     { kind: "github", url: "https://github.com/LAGameStudio/InputCandy" },
      { kind: "discord", url: "https://discord.gg/9vkNPJr" },
    ],
    paragraph: "Awesome gamepad support for GameMaker Studio 2 games. InputCandy focuses on providing input testing and customization for users of PC and gamepad-friendly platforms (consoles). It's that classic 'control setup' panel system you are accustomed to seeing in video games small and large, but for GameMaker. It also is an object-oriented input API, with action matching, and a bunch of device detection features for keyboard, mouse and any SDL controller. For GMS 2.3.1+"
@@ -25,8 +26,8 @@ let indiedevs = [
    channel: "#lost-astronaut",
    linkback: "https://lostastronaut.com/apolune2", 
    links: [
-     { kind: "discord", url: "https://discord.gg/9vkNPJr" },
      { kind: "steam", url: "https://store.steampowered.com/app/1549140/Apolune_2/" },
+     { kind: "discord", url: "https://discord.gg/9vkNPJr" },
    ],
    paragraph: "Apolune 2 is a hilarious and shocking resource management game where one player (or more) experience life on an asteroid mining team in deep space. Tethered to a heavy space station, the game evolves as you mine ore for money, trade and upgrade your station, alone or with your friends."
  },
