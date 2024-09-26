a79544fee446fe9167a60833a3b9bfdc7ca84dd2
h3rb <2787368+h3rb@users.noreply.github.com>
2024-09-25 21:55:05 -0400

    Update data.js

--- data.js
+++ data.js
@@ -2,6 +2,53 @@
 let indiedevs = [
 
   /* Try using the template found in the Readme here: https://github.com/LAGameStudio/lagamestudio.github.io/ */
+
+
+ { 
+   kind: "game",
+   title: "SkyFrizz",
+   image: "images/Frizz.png",   
+   date: "9-25-2024",
+   location: "United States",
+   channel: "#liminalist-contraptions",
+   linkback: "https://liminalist-contraptions.itch.io/sky-frizz", 
+   links: [
+     { kind: "itch", url: "https://liminalist-contraptions.itch.io/sky-frizz" }
+   ],
+   paragraph: "A sci fi action/adventure platformer with procedurally generated levels. Play as a freelance intergalactic secret agent Sky Frizz and see what the evil Shade Bots are up to on planet Aphid. Collect items to help you traverse this randomized world, including jetpacks, quake mines, force fields, and Sky's signature tool/weapon the Sky Hook! There may be future updates (bug fixes etc.), but this is the complete game. There is only a windows version available at this time. A unique Atari VCS version is now available!"
+ },
+  
+ { 
+   kind: "game",
+   title: "Maze Lab",
+   image: "images/MazeLab.png",   
+   date: "9-25-2024",
+   location: "United States",
+   channel: "#liminalist-contraptions",
+   linkback: "https://liminalist-contraptions.itch.io/maze-lab", 
+   links: [
+     { kind: "itch", url: "https://liminalist-contraptions.itch.io/maze-lab" }
+   ],
+   paragraph: "An old school 3d maze puzzle game. Play through the selection of challenging mazes provided. Watch out for lasers, falling blades, and poisonous gas! Build your own puzzle mazes and let friends play! Use a selection of puzzle elements, traps, items, and visual themes. The game is silent, yet an alternate soundtrack is available for download including the music of Liminalist Trixels, No Lake, and Loop Based Zero."
+ },
+  
+ { 
+   kind: "company",
+   title: "Liminalist Contraptions",
+   image: "images/Limin.jpg",   
+   date: "9-25-2024",
+   location: "Massachusetts, United States",
+   channel: "#liminalist-contraptions",
+   linkback: "https://distrokid.com/hyperfollow/obsidiancontraption/the-great-work", 
+   links: [
+     { kind: "itch", url: "https://liminalist-contraptions.itch.io/" },
+     { kind: "twitter", url: "https://twitter.com/Uncle_Flip1" },
+     { kind: "youtube", url: "https://www.youtube.com/c/ObsidianContraption" },
+     { kind: "link", url: "https://distrokid.com/hyperfollow/obsidiancontraption/the-great-work" }
+   ],
+   paragraph: "Liminalist Contraptions is my game studio, creator of Sky Frizz. I am a musician, game developer &amp; OST director. Obsidian Contraption is my main music project. An experimental electronic group spanning genres, from industrial, hip hop to darkwave &amp; chiptune."
+ },
+  
  { 
    kind: "tool",
    title: "InputCandy",
@@ -31,7 +78,7 @@ let indiedevs = [
    ],
    paragraph: "Apolune 2 is a hilarious and shocking resource management game where one player (or more) experience life on an asteroid mining team in deep space. Tethered to a heavy space station, the game evolves as you mine ore for money, trade and upgrade your station, alone or with your friends."
  },
-  
+    
  { 
    kind: "company",
    title: "Lost Astronaut Studios",
