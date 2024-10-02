1f3c593307ae19fb33168f3f5fb1658dd3bef589
h3rb <2787368+h3rb@users.noreply.github.com>
2024-10-02 14:21:17 -0400

    Update data.js

--- data.js
+++ data.js
@@ -3,6 +3,49 @@ let indiedevs = [
 
   /* Try using the template found in the Readme here: https://github.com/LAGameStudio/lagamestudio.github.io/ */
 
+ {
+    kind: "company",
+    title: "Cuirass Entertainment Ltd.",
+    image: "images/Cuirass-Entertainment-Ltd.png",
+    date: "10-2-2024",
+    location: "Nova Scotia",
+    channel: "#cuirass-entertainment",
+    linkback: "https://cuirass.net/",
+    links: [
+      { kind: "discord", url: "https://discord.gg/jmkMsevDqA" }
+    ],
+    paragraph: "Cuirass Entertainment Ltd creates ambitious games on a light budget. Modern game development for the global audience. Player focused, fun driven games that emphasize value and customer friendly practises. We develop games to positively impact our player's lives. Whether it be through the challenge, the education, or the story, we aim to design timeless experiences for a wide range of unique and diverse players."
+  },
+
+  {
+    kind: "game",
+    title: "End's Reach",
+    image: "images/Cuirass-Entertainment-Ltd-Ends-Reach.png",
+    date: "10-2-2024",
+    location: "Nova Scotia",
+    channel: "#cuirass-entertainment",
+    linkback: "https://endsreach.com/",
+    links: [
+      { kind: "steam", url: "https://store.steampowered.com/app/2499330/Battle_Rockets/" },
+      { kind: "itch", url: "https://gumbomachine.itch.io/battle-rockets" }
+    ],
+    paragraph: "End's Reach is a science fiction action-adventure platformer which follows the endeavours of a man who wakes up lost in deep space with a broken suit and a shattered mind, driven by nothing but the memory of someone he seeks to locate. End's Reach aims to recapture the joy of the 3D action platformers from decades past, but with a new spin. Modernized gameplay built and adapted specifically for the Atari VCS."
+  },
+
+  {
+    kind: "game",
+    title: "Carrier",
+    image: "images/Cuirass-Entertainment-Ltd-Carrier.png",
+    date: "10-2-2024",
+    location: "Nova Scotia",
+    channel: "#cuirass-entertainment",
+    linkback: "https://store.steampowered.com/app/614250/Carrier/",
+    links: [
+      { kind: "steam", url: "https://store.steampowered.com/app/614250/Carrier/" },
+    ],
+    paragraph: "Carrier is a Free-to-Play timing-based puzzle game. It puts you in control of an operator who must modify and encrypt a transmission signal to avoid interception. Using simplistic two-button controls you'll guide the encrypted radio waves past the 'Scramblers' through 30 different levels spanning across 5 worlds."
+  },
+  
   {
     kind: "company",
     title: "Gumbo Machine",
